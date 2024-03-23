ec6bd74aeca75df2c2d0b309a192ac5dd1913b82
reshmic202 <reshmic202@gmail.com>
2024-03-23 14:47:12 +0530

    first commit

---
+++ Bank_Application/Main.java
@@ -0,0 +1,10 @@
+package Bank_Application;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Operation operation = new Operation();
+        operation.bankinfo();
+    }
+}
